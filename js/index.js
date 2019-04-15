9489e84db4f0771771b1acb25a6e0b8d7210966f
yohei.yamana <fixeight@gmail.com>
2019-04-15 17:22:01 +0900

    add js example

---
+++ js/index.js
@@ -0,0 +1,39 @@
+const ref = require('ref');
+const ffi = require('ffi');
+
+const p_graal_isolatethread_t = ref.alloc(ref.refType(ref.types.void))
+
+const libJava = ffi.Library('../libnativeimpl', {
+  graal_create_isolate: [
+    ref.types.int, [
+      ref.refType(ref.types.void),
+      ref.refType(ref.types.void),
+      ref.refType(ref.refType(ref.types.void))
+    ]],
+  Java_org_pkg_apinative_Native_add: [
+    ref.types.int,
+    [ref.refType(ref.types.void), ref.types.int, ref.types.int]],
+  Java_org_pkg_apinative_Native_hello: [
+    ref.types.CString,
+    [ref.refType(ref.types.void)]],
+  Java_org_pkg_apinative_Native_rptstr: [
+    ref.types.CString, [
+      ref.refType(ref.types.void),
+      ref.refType(ref.types.CString),
+      ref.types.int
+    ]],
+})
+
+const rc = libJava.graal_create_isolate(ref.NULL, ref.NULL, p_graal_isolatethread_t)
+if (rc !== 0) {
+  console.log('graal_create_isolate error')
+}
+
+const added = libJava.Java_org_pkg_apinative_Native_add(ref.deref(p_graal_isolatethread_t), 11, 22)
+console.log(added)
+
+const hello = libJava.Java_org_pkg_apinative_Native_hello(ref.deref(p_graal_isolatethread_t))
+console.log(hello)
+
+const rep = libJava.Java_org_pkg_apinative_Native_rptstr(ref.deref(p_graal_isolatethread_t), ref.allocCString("Hello"), 10)
+console.log(rep)
